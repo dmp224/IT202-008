4b265bb6cf1201e60fbbd154426eb995bccebecb
Dileesha Patel <dmp224@njit.edu>
2023-04-17 17:28:03 -0400

    change

--- lib/functions.php
+++ lib/functions.php
@@ -1,6 +1,12 @@
 <?php
+//TODO 1: require db.php
 require_once(__DIR__ . "/db.php");
+//This is going to be a helper for redirecting to our base project path since it's nested in another folder
+//This MUST match the folder name exactly
 $BASE_PATH = '/Project/';
+//TODO 4: Flash Message Helpers
+require(__DIR__ . "/flash_messages.php");
+
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
 //TODO 2: filter helpers
@@ -8,28 +14,10 @@ require(__DIR__ . "/sanitizers.php");
 
 //TODO 3: User helpers
 require(__DIR__ . "/user_helpers.php");
-//TODO 4: Flash Message Helpers
-require(__DIR__ . "/flash_message.php");
 
 
-if (!$hasError){
-    $hash = password_hash($password, PASSWORD_BCRYPT);
-    $db = getDB();
-    $stmt = $db->prepare("INSERT INTO Users(email, password) VALUES (:email, :password)");
-    try{
-        $r = $stmt->execute([":email" => $email, ":password" => $hash]);
-        echo "Successfully register!";
-    }   catch (Exception $e){
-            echo "There was an error registering<br>";
-            echo "<pre>" . var_export($e, true) . "</pre>";
-    }
-    
-}
-
-
-
-
-
-
-
+//duplicate email/username
+require(__DIR__ . "/duplicate_user_details.php");
+//reset session
+require(__DIR__ . "/reset_session.php");
 ?>
--- partials/nav.php
+++ partials/nav.php
@@ -1,4 +1,5 @@
 <?php
+require_once(__DIR__ . "/../lib/functions.php");
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
@@ -11,7 +12,7 @@ $localWorks = true; //some people have issues with localhost for the cookie para
 if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
-        "path" => "/Project",
+        "path" => "$BASE_PATH",
         //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
         "domain" => $domain,
         "secure" => true,
@@ -20,4 +21,24 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     ]);
 }
 session_start();
-require_once(__DIR__ . "/../lib/functions.php");
+
+
+?>
+<!-- include css and js files -->
+<link rel="stylesheet" href="styles.css">
+<script src="helpers.js"></script>
+<nav>
+    <ul>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
+</nav>
