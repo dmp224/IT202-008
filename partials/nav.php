36fac31bc52a52350ddf362b81f06b3ebac143a3
Dileesha Patel <dmp224@njit.edu>
2023-04-25 14:17:16 -0400

    errors

--- partials/nav.php
+++ partials/nav.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <?php
 require_once(__DIR__ . "/../lib/functions.php");
 //Note: this is to resolve cookie issues with port numbers
@@ -27,31 +25,20 @@ session_start();
 
 ?>
 <!-- include css and js files -->
-<link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
-<script src="<?php echo get_url('helpers.js'); ?>"></script>
+<link rel="stylesheet" href="styles.css">
+<script src="helpers.js"></script>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
-<<<<<<< HEAD
             <li><a href="home.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
-=======
-            <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
-            <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
->>>>>>> 5cbcd433cf04da1b6730dc2d803ff78bd4fe0a30
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
-            <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
-        <?php endif; ?>
-        <?php if (has_role("Admin")) : ?>
-            <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
-            <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
-            <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+            <li><a href="logout.php">Logout</a></li>
         <?php endif; ?>
     </ul>
 </nav>
->>>>>>> 07241b6b23021430f4daabd1cbc26051c258267e
