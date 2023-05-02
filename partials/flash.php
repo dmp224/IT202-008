0df1559d0cba36acdf15c997b26894f8f69a8af4
Dileesha Patel <dmp224@njit.edu>
2023-05-03 00:25:18 +0300

    Milestone 2 deliverables

--- partials/flash.php
+++ partials/flash.php
@@ -2,7 +2,7 @@
 /*put this at the bottom of the page so any templates
  populate the flash variable and then display at the proper timing*/
 ?>
-<div class="container" id="flash">
+<div class="row flash-row" id="flash">
     <?php $messages = getMessages(); ?>
     <?php if ($messages) : ?>
         <?php foreach ($messages as $msg) : ?>
@@ -15,7 +15,7 @@
 <script>
     //used to pretend the flash messages are below the first nav element
     function moveMeUp(ele) {
-        let target = document.getElementsByTagName("nav")[0];
+        let target = document.getElementsByTagName("navbarr")[0];
         if (target) {
             target.after(ele);
         }
